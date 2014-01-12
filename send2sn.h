7b8086530438b6dc86196b1ec3d6c3cc252c2c2f
newmlee <matthewlee0725@gmail.com>
2014-01-12 00:33:50 +0000

    头文件

---
+++ send2sn.h
@@ -0,0 +1,20 @@
+#ifndef __SEND2SN__
+#define __SEND2SN__
+
+#include <gtk/gtk.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <limits.h>
+
+struct Item {
+    GtkWidget *text_view;
+    GtkWidget *renren_button;
+    GtkWidget *qzone_button;
+    GtkWidget *weibo_button;
+    GtkWidget *ok_button;
+};
+
+#endif
+
