c32a5de76acd9c3d2894a052577d224ea1a52d6e
newmlee <matthewlee0725@gmail.com>
2014-01-12 23:30:40 +0000

    帮助

--- help_about.c
+++ help_about.c
@@ -9,7 +9,7 @@ void help_about(GtkWidget *widget, gpointer parents)
     gtk_about_dialog_set_version(GTK_ABOUT_DIALOG(dialog), "0.9");
     gtk_about_dialog_set_copyright(GTK_ABOUT_DIALOG(dialog), "(c) Matthew Lee");
     gtk_about_dialog_set_comments(GTK_ABOUT_DIALOG(dialog),
-                                  "Send2SN是一款简易的班级信息推送应用，可以将班级信息发送至人人网、QQ空间和新浪微博。采用GTK+2.24开发。");
+                                  "Send2SN是一款简易的班级信息推送应用，可以将班级信息发送至班级成员邮箱、微信公众平台和人人网。\n采用GTK+2.24、C以及Python开发。");
     gtk_about_dialog_set_website(GTK_ABOUT_DIALOG(dialog),
                                  "https://github.com/newmlee/Send2SN");
 
