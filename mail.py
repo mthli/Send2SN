4210acdba36e6b26f56c51fcd6971c1635b7634e
newmlee <matthewlee0725@gmail.com>
2014-01-13 15:26:42 +0000

    群发邮件

--- mail.py
+++ mail.py
@@ -2,38 +2,40 @@
 # Filename: mail.py
 # Coding: utf-8
 
-from smtplib         import Smtp
+from smtplib         import SMTP_SSL
 from email.header    import Header
-from email.mime.text import Text
+from email.mime.text import MIMEText
+import datetime
 
-# 默认设置为使用Gmail，若需要修改可参考“帮助”——“指南”
-# 发信人设置：
-# sender   = '你的邮箱帐号'
-# passwd   = '你的邮箱密码'
-# classid  = '你的班级号码'
-sender     = ' '
-passwd     = ' '
-classid    = ' '
+# Gmail is default mail sender's system.
+# If you want to use another mail system, see "menubar"-"help"-"guide"
+# sender   = 'your mail count'
+# passwd   = 'your mail password'
+# classid  = 'your class number'
+sender     = ''
+passwd     = ''
+classid    = ''
 
-# 按照如下格式添加收信人邮箱帐号：
+# follow this form to add your receivers:
 # receivers = (
-#   '第1个邮箱地址',
-#   '第2个邮箱地址',
+#   'aaaaaa@gmail.com',
+#   'bbbbbb@gmail.com',
 #   ...
-#   '第n个邮箱地址
+#   'zzzzzz@gmail.com'
 # )
 receivers = (
-    ' ', 
-    ' '
+    '', 
+    ''
 )
 
 infile = open('message.txt')
 content = infile.read()
+content = content.rstrip()
 infile.close()
-message = Text(content, _charset = 'utf-8')
-message['Subject'] = Header(classid + '通知', 'utf-8')
+message = MIMEText(content, _charset = 'utf-8')
+message['Subject'] = Header(classid + ' ' + str(datetime.datetime.now()) + ' Notice', 'utf-8')
 
-smtp = SMTP('smtp.gmail.com', 465, timeout = 10)
+smtp = SMTP_SSL('smtp.gmail.com', 465, timeout = 10)
 
 try:
     smtp.login(sender, passwd)
