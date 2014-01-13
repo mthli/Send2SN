7c0ba7b52d6e6fc73363cb6697e7baf98cb29a2d
newmlee <matthewlee0725@gmail.com>
2014-01-13 15:26:14 +0000

    发送到人人网

--- renren.py
+++ renren.py
@@ -7,12 +7,11 @@ import urllib2
 import re
 import cookielib
 
-# sender = '你的人人帐号'
-# passwd = '你的人人密码'
-sender = ' '
-passwd = ' '
+# sender = 'your renren id'
+# passwd = 'your renren password'
+sender = ''
+passwd = ''
 
-# 登陆人人网
 def login(sender, passwd):
     data = {'email': sender, 'password': passwd}
     url_data = urllib.urlencode(data);
@@ -26,7 +25,6 @@ def login(sender, passwd):
     uid = re.search("'ruid':'(\d+)'", html).group(1)
     return (opener, uid)
 
-# 更新状态，content需要从文本中读取，或者使用Python.h
 def status(sender, passwd):
     l = login(sender, passwd)
     opener = l[0]
@@ -37,10 +35,10 @@ def status(sender, passwd):
     rtk = re.search("get_check_x:'(\w+)'", html).group(1)
     infile = open('message.txt')
     message = infile.read()
+    message = message.rstrip()
     infile.close()
     data = {"content": message, "hostid": uid, "requestToken": rt, "_rtk": rtk, "channel": "renren"}
     url_data = urllib.urlencode(data)
     res = opener.open("http://shell.renren.com/"+uid+"/status", url_data)
 
-# 执行程序
 status(sender, passwd)
