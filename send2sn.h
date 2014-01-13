ea54cf8aeb6324cf64af7d2686753f6edc175d5b
newmlee <matthewlee0725@gmail.com>
2014-01-13 15:29:35 +0000

    头文件

--- send2sn.h
+++ send2sn.h
@@ -5,16 +5,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <time.h>
-#include <limits.h>
 
-struct Item {
-    GtkWidget *text_view;
+typedef struct _Item {
+    GtkWidget *mail_button;
+    GtkWidget *weixin_button;
     GtkWidget *renren_button;
-    GtkWidget *qzone_button;
-    GtkWidget *weibo_button;
-    GtkWidget *ok_button;
-};
+    GtkTextBuffer *buffer;
+}
+Item;
 
 #endif
-
